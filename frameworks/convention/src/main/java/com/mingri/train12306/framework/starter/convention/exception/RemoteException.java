1628f0e5116b62f289f149015dda15b01647d571
mingri31164 <3116430062@qq.com>
2025-02-19 23:36:37 +0800

    封装规约组件库

---
+++ frameworks/convention/src/main/java/com/mingri/train12306/framework/starter/convention/exception/RemoteException.java
@@ -0,0 +1,24 @@
+package com.mingri.train12306.framework.starter.convention.exception;
+
+import com.mingri.train12306.framework.starter.convention.errorcode.BaseErrorCode;
+import com.mingri.train12306.framework.starter.convention.errorcode.IErrorCode;
+import lombok.ToString;
+
+/**
+ * 远程服务调用异常
+ */
+@ToString
+public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
+
+    public RemoteException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public RemoteException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+}
