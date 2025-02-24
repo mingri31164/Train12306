ea2b6df0249d9df058fd325e55beafa3db5d325a
mingri31164 <3116430062@qq.com>
2025-02-24 20:36:50 +0800

    完成网关服务

---
+++ services/gateway-service/src/main/java/com/mingri/train12306/biz/gatewayservice/toolkit/JWTUtil.java
@@ -0,0 +1,47 @@
+package com.mingri.train12306.biz.gatewayservice.toolkit;
+
+import com.alibaba.fastjson2.JSON;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+
+/**
+ * JWT 工具类
+ */
+@Slf4j
+public final class JWTUtil {
+
+    static {
+        KEY = System.getenv("JWT_KEY");
+    }
+
+    public static final String KEY;
+
+    /**
+     * 解析用户 Token
+     *
+     * @param jwtToken 用户访问 Token
+     * @return 用户信息
+     */
+    public static UserInfoDTO parseJwtToken(String jwtToken) {
+        if (StringUtils.hasText(jwtToken)) {
+            try {
+                Claims claims = Jwts.parser().setSigningKey(KEY).parseClaimsJws(jwtToken).getBody();
+                Date expiration = claims.getExpiration();
+                if (expiration.after(new Date())) {
+                    String subject = claims.getSubject();
+                    return JSON.parseObject(subject, UserInfoDTO.class);
+                }
+            } catch (ExpiredJwtException ignored) {
+            } catch (Exception ex) {
+                log.error("JWT Token解析失败，请检查", ex);
+            }
+        }
+        return null;
+    }
+}
