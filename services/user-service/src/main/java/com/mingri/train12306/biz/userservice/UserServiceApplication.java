d1ec1e4d1d47beb0f9353f834dcde41f5f40eb2c
mingri31164 <3116430062@qq.com>
2025-02-25 03:31:45 +0800

    新增用户服务模块

---
+++ services/user-service/src/main/java/com/mingri/train12306/biz/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mingri.train12306.biz.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
