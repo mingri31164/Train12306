b4cf52d330092638bb3fdb899fbee96708822fa5
mingri31164 <3116430062@qq.com>
2025-02-27 06:39:12 +0800

    定义车票服务数据持久层

---
+++ services/ticket-service/src/main/java/com/mingri/train12306/biz/ticketservice/dao/mapper/RegionMapper.java
@@ -0,0 +1,10 @@
+package com.mingri.train12306.biz.ticketservice.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mingri.train12306.biz.ticketservice.dao.entity.RegionDO;
+
+/**
+ * 地区持久层
+ */
+public interface RegionMapper extends BaseMapper<RegionDO> {
+}
