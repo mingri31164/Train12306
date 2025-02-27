cc8da2b512617ac8a96c3f71209df4c0e7c6ae70
mingri31164 <3116430062@qq.com>
2025-02-28 04:48:58 +0800

    封装车票信息工具类

--- frameworks/designpattern/src/main/java/com/mingri/train12306/framework/starter/designpattern/chain/AbstractChainHandler.java
+++ frameworks/designpattern/src/main/java/com/mingri/train12306/framework/starter/designpattern/chain/AbstractChainHandler.java
@@ -4,6 +4,8 @@ import org.springframework.core.Ordered;
 
 /**
  * 抽象业务责任链组件
+ * Ordered 接口通常用于定义组件的顺序或优先级。
+ * 实现 Ordered 接口的类需要实现 getOrder() 方法，返回一个整数值，表示该组件的优先级。数值越小优先级越高
  */
 public interface AbstractChainHandler<T> extends Ordered {
 
