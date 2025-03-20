6b3863ee0a2fb70056a98dfd02ebd0bc13224d3d
mingri31164 <3116430062@qq.com>
2025-03-20 22:45:08 +0800

    用户注册布隆过滤器误判处理

--- services/user-service/src/main/java/com/mingri/train12306/biz/userservice/service/filter/impl/UserRegisterHasUsernameChainHandler.java
+++ services/user-service/src/main/java/com/mingri/train12306/biz/userservice/service/filter/impl/UserRegisterHasUsernameChainHandler.java
@@ -1,12 +1,17 @@
 package com.mingri.train12306.biz.userservice.service.filter.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.mingri.train12306.biz.userservice.common.enums.UserRegisterErrorCodeEnum;
+import com.mingri.train12306.biz.userservice.dao.entity.UserDO;
+import com.mingri.train12306.biz.userservice.dao.mapper.UserMapper;
 import com.mingri.train12306.biz.userservice.dto.req.UserRegisterReqDTO;
 import com.mingri.train12306.biz.userservice.service.filter.UserRegisterCreateChainFilter;
+import com.mingri.train12306.biz.userservice.service.impl.UserServiceImpl;
 import com.mingri.train12306.framework.starter.cache.DistributedCache;
 import com.mingri.train12306.framework.starter.convention.exception.ClientException;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +27,8 @@ public final class UserRegisterHasUsernameChainHandler implements UserRegisterCr
 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
     private final DistributedCache distributedCache;
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     public void handler(UserRegisterReqDTO requestParam) {
@@ -29,8 +36,17 @@ public final class UserRegisterHasUsernameChainHandler implements UserRegisterCr
         if (userRegisterCachePenetrationBloomFilter.contains(username)) {
             // 布隆过滤器存在该用户名
             StringRedisTemplate instance = (StringRedisTemplate) distributedCache.getInstance();
-            if (Boolean.FALSE.equals(instance.opsForSet().isMember(USER_REGISTER_REUSE_SHARDING + hashShardingIdx(username), username)))
-                throw new ClientException(UserRegisterErrorCodeEnum.HAS_USERNAME_NOTNULL);
+
+            // 判断数据库中是否存在该用户名
+            QueryWrapper<UserDO> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("username", username);
+
+            // 判断数据库中是否存在该用户名
+            if (userMapper.exists(queryWrapper)) {
+                // 如果 Redis 中不存在该用户名，则抛出异常
+                if (Boolean.FALSE.equals(instance.opsForSet().isMember(USER_REGISTER_REUSE_SHARDING + hashShardingIdx(username), username)))
+                    throw new ClientException(UserRegisterErrorCodeEnum.HAS_USERNAME_NOTNULL);
+            }
         }
     }
 
